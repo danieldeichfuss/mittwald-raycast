72571245fa0bce3da0c242fcf74752591763f755
Daniel Deichfuß <d.deichfuss@gmail.com>
2024-08-22 21:56:31 +0200

    Add organizations command

---
+++ src/view-mittwald-organizations.tsx
@@ -0,0 +1,43 @@
+import { ActionPanel, Action, Icon, List, getPreferenceValues } from "@raycast/api";
+import { useFetch } from "@raycast/utils";
+
+interface Preferences {
+  token: string;
+}
+
+export default function Command() {
+  const preferences = getPreferenceValues<Preferences>();
+
+  const { isLoading, data: organizations } = useFetch<{ name: string; customerId: string }[]>(
+    "https://api.mittwald.de/v2/customers",
+    {
+      method: "get",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${preferences.token}`,
+      },
+    },
+  );
+
+  return (
+    <List isLoading={!organizations && !isLoading}>
+      {organizations?.map((organizations) => (
+        <List.Item
+          key={organizations.customerId}
+          icon={Icon.CheckCircle}
+          title={organizations.name}
+          actions={
+            <ActionPanel>
+              <Action.OpenInBrowser
+                url={`https://studio.mittwald.de/app/organizations/${organizations.customerId}/dashboard`}
+              ></Action.OpenInBrowser>
+              <Action.CopyToClipboard
+                content={`https://studio.mittwald.de/app/organizations/${organizations.customerId}/dashboard`}
+              />
+            </ActionPanel>
+          }
+        />
+      ))}
+    </List>
+  );
+}
