335cdd7abc25a2b5edccac318fd49bb62eab64e6
Daniel Deichfuß <d.deichfuss@gmail.com>
2024-08-22 21:55:39 +0200

    Cleanup project command

--- src/view-mittwald-projects.tsx
+++ src/view-mittwald-projects.tsx
@@ -8,32 +8,29 @@ interface Preferences {
 export default function Command() {
   const preferences = getPreferenceValues<Preferences>();
 
-  console.log({ preferences });
-
-  const { isLoading, data } = useFetch<{ customerId: string; description: string; id: string; enabled: boolean }[]>(
-    "https://api.mittwald.de/v2/projects",
-    {
-      method: "get",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${preferences.token}`,
-      },
+  const { isLoading, data: projects } = useFetch<
+    { customerId: string; description: string; id: string; enabled: boolean }[]
+  >("https://api.mittwald.de/v2/projects", {
+    method: "get",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${preferences.token}`,
     },
-  );
+  });
 
   return (
-    <List isLoading={!data && !isLoading}>
-      {data?.map((item) => (
+    <List isLoading={!projects && !isLoading}>
+      {projects?.map((project) => (
         <List.Item
-          key={item.id}
-          icon={item.enabled ? Icon.CheckCircle : Icon.XMarkCircle}
-          title={item.description}
+          key={project.id}
+          icon={project.enabled ? Icon.CheckCircle : Icon.XMarkCircle}
+          title={project.description}
           actions={
             <ActionPanel>
               <Action.OpenInBrowser
-                url={`https://studio.mittwald.de/app/projects/${item.id}/dashboard`}
+                url={`https://studio.mittwald.de/app/projects/${project.id}/dashboard`}
               ></Action.OpenInBrowser>
-              <Action.CopyToClipboard content={item.description} />
+              <Action.CopyToClipboard content={`https://studio.mittwald.de/app/projects/${project.id}/dashboard`} />
             </ActionPanel>
           }
         />
